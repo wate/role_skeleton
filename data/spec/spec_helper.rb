baa59d3e66de0352ec0cbb793dd591d67722c3f6
wate <pc.poisoning@gmail.com>
2017-12-17 17:50:11 +0900

    update

--- data/spec/spec_helper.rb
+++ data/spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require 'serverspec'
 require 'net/ssh'
+require 'yaml'
 
 host = ENV['TARGET_HOST']
 ssh_config_files = ['./.vagrant/ssh-config'] + Net::SSH::Config.default_files
@@ -10,6 +11,31 @@ options[:keys].push("#{Dir.home}/.vagrant.d/insecure_private_key")
 set :backend, :ssh
 set :host, host
 set :ssh_options, options
+
+spec_dir = File.dirname(__FILE__)
+role_dir = File.dirname(spec_dir)
+
+test_vars = {}
+var_file = File.join(role_dir, 'defaults', 'main.yml')
+if File.exist?(var_file)
+  merge_vars = YAML.load_file(var_file)
+  test_vars.merge!(merge_vars) if merge_vars.is_a?(Hash)
+end
+
+molecule_config = YAML.load_file(File.join(role_dir, 'molecule.yml'))
+if molecule_config.key?('ansible') && molecule_config['ansible'].key?('group_vars')
+  if molecule_config['ansible']['group_vars'].key?('all')
+    test_vars.merge!(molecule_config['ansible']['group_vars']['all'])
+  end
+end
+
+var_file = File.join(role_dir, 'vars', 'main.yml')
+if File.exist?(var_file)
+  merge_vars = YAML.load_file(var_file)
+  test_vars.merge!(merge_vars) if merge_vars.is_a?(Hash)
+end
+
+set_property test_vars
 
 RSpec.configure do |config|
   config.color = true
