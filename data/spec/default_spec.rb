f1b9f7e6f3d1a8ed7b3797da45bf75c69140adcd
wate <pc.poisoning@gmail.com>
2018-03-05 01:03:25 +0900

    serverspec周りの設定を調整

--- data/spec/default_spec.rb
+++ data/spec/default_spec.rb
@@ -1,7 +1,59 @@
 require 'spec_helper'
 
-describe file('/etc/hosts') do
+describe package('httpd') do
+  it { should be_installed }
+end
+
+describe file('/path/to/file') do
+  it { should exist }
   it { should be_file }
-  it { should be_owned_by 'root' }
-  it { should be_grouped_into 'root' }
+  its(:content) { should match(/ServerName www.example.jp/) }
+  it { should contain 'ServerName www.example.jp' }
+  it { should contain('rspec').from(/^group :test do/).to(/^end/) }
+  it { should contain('rspec').after(/^group :test do/) }
+  it { should contain('rspec').before(/^end/) }
+  it { should be_directory }
+  it { should be_readable }
+  it { should be_writable }
+  it { should be_symlink }
+  it { should be_linked_to '/etc/redhat-release' }
+  it { should be_executable }
+  it { should be_mode 440 }
+end
+
+describe service('ntpd') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe command('apachectl -V') do
+  its(:stdout) { should match(/Apache/) }
+  its(:stdout) { should contain('Prefork') }
+  its(:stdout) { should contain('Prefork').from(/^Server MPM/).to(/^Server compiled/) }
+  its(:stdout) { should contain('conf/httpd.conf').after('SERVER_CONFIG_FILE') }
+  its(:stdout) { should contain(' Apache/2.2.29').before('Server built') }
+  its(:stderr) { should match(/apachectl: command not found/) }
+  its(:exit_status) { should eq 0 }
+end
+
+describe user('apache') do
+  it { should exist }
+  it { should belong_to_group 'apache' }
+  it { should belong_to_primary_group 'apache' }
+  it { should have_uid 0 }
+  it { should have_home_directory '/root' }
+  it { should have_login_shell '/bin/bash' }
+  it { should have_authorized_key 'ssh-rsa ABCDEFGHIJKLMNOPQRSTUVWXY..... foo@bar.local' }
+end
+
+describe group('wheel') do
+  it { should exist }
+  it { should have_gid 0 }
+end
+
+describe process('memcached') do
+  its(:user) { should eq 'memcached' }
+  its(:args) { should match(/-c 32000\b/) }
+  its(:count) { should eq 1 }
+  it { should be_running }
 end
--- data/spec/spec_helper.rb
+++ data/spec/spec_helper.rb
@@ -12,28 +12,35 @@ set :backend, :ssh
 set :host, host
 set :ssh_options, options
 
+def e(value)
+  Regexp.escape(value.is_a?(String) ? value : value.to_s)
+end
+
 spec_dir = File.dirname(__FILE__)
 role_dir = File.dirname(spec_dir)
 
 test_vars = {}
-var_file = File.join(role_dir, 'defaults', 'main.yml')
-if File.exist?(var_file)
-  merge_vars = YAML.load_file(var_file)
-  test_vars.merge!(merge_vars) if merge_vars.is_a?(Hash)
-end
 
-molecule_config = YAML.load_file(File.join(role_dir, 'molecule.yml'))
-if molecule_config.key?('ansible') && molecule_config['ansible'].key?('group_vars')
-  if molecule_config['ansible']['group_vars'].key?('all')
-    test_vars.merge!(molecule_config['ansible']['group_vars']['all'])
+var_file = File.join(role_dir, 'defaults', 'main.yml')
+test_vars.merge!(YAML.load_file(var_file)) if File.exist?(var_file)
+
+group_names = ['all']
+
+var_file = File.join(role_dir, '.molecule', 'facts', host + '.yml')
+if File.exist?(var_file)
+  test_vars.merge!(YAML.load_file(var_file))
+  if test_vars.key?('group_names')
+    group_names = test_vars['group_names'].unshift('all')
   end
 end
 
-var_file = File.join(role_dir, 'vars', 'main.yml')
-if File.exist?(var_file)
-  merge_vars = YAML.load_file(var_file)
-  test_vars.merge!(merge_vars) if merge_vars.is_a?(Hash)
+group_names.each do |name|
+  var_file = File.join(role_dir, '.molecule', 'group_vars', name)
+  test_vars.merge!(YAML.load_file(var_file)) if File.exist?(var_file)
 end
+
+var_file = File.join(role_dir, 'vars', 'main.yml')
+test_vars.merge!(YAML.load_file(var_file)) if File.exist?(var_file)
 
 set_property test_vars
 
