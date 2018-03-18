7c39ca43a58c2a1c3697368e81ace0cc0d96fa10
wate <pc.poisoning@gmail.com>
2018-03-18 18:03:53 +0900

    update

--- data/spec/spec_helper.rb
+++ data/spec/spec_helper.rb
@@ -16,13 +16,30 @@ def e(value)
   Regexp.escape(value.is_a?(String) ? value : value.to_s)
 end
 
+class ::Hash
+  def deep_merge(other_hash, &block)
+    dup.deep_merge!(other_hash, &block)
+  end
+  def deep_merge!(other_hash, &block)
+    other_hash.each_pair do |k,v|
+      tv = self[k]
+      if tv.is_a?(Hash) && v.is_a?(Hash)
+        self[k] = tv.deep_merge(v, &block)
+      else
+        self[k] = block && tv ? block.call(k, tv, v) : v
+      end
+    end
+    self
+  end
+end
+
 spec_dir = File.dirname(__FILE__)
 role_dir = File.dirname(spec_dir)
 
 test_vars = {}
 
 var_file = File.join(role_dir, 'defaults', 'main.yml')
-test_vars.merge!(YAML.load_file(var_file)) if File.exist?(var_file)
+test_vars.deep_merge!(YAML.load_file(var_file)) if File.exist?(var_file)
 
 group_names = ['all']
 
@@ -36,11 +53,11 @@ end
 
 group_names.each do |name|
   var_file = File.join(role_dir, '.molecule', 'group_vars', name)
-  test_vars.merge!(YAML.load_file(var_file)) if File.exist?(var_file)
+  test_vars.deep_merge!(YAML.load_file(var_file)) if File.exist?(var_file)
 end
 
 var_file = File.join(role_dir, 'vars', 'main.yml')
-test_vars.merge!(YAML.load_file(var_file)) if File.exist?(var_file)
+test_vars.deep_merge!(YAML.load_file(var_file)) if File.exist?(var_file)
 
 set_property test_vars
 
