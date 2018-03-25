a073f96d8d16c9ba192e44b087ab970694619209
wate <pc.poisoning@gmail.com>
2018-03-25 18:14:29 +0900

    update

--- data/spec/spec_helper.rb
+++ data/spec/spec_helper.rb
@@ -20,17 +20,16 @@ class ::Hash
   def deep_merge(other_hash, &block)
     dup.deep_merge!(other_hash, &block)
   end
-
   def deep_merge!(other_hash, &block)
-    other_hash.each_pair do |k, v|
-      tv = self[k]
-      if tv.is_a?(Hash) && v.is_a?(Hash)
-        self[k] = tv.deep_merge(v, &block)
+    merge!(other_hash) do |key, this_val, other_val|
+      if this_val.is_a?(Hash) && other_val.is_a?(Hash)
+        this_val.deep_merge(other_val, &block)
+      elsif block_given?
+        block.call(key, this_val, other_val)
       else
-        self[k] = block && tv ? block.call(k, tv, v) : v
+        other_val
       end
     end
-    self
   end
 end
 
