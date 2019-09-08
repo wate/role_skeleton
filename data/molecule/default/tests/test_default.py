749db66e27d773cfadc7ef935039901a40df3662
wate <pc.poisoning@gmail.com>
2019-09-09 02:59:51 +0900

    update

--- data/molecule/default/tests/test_default.py
+++ data/molecule/default/tests/test_default.py
@@ -1,7 +1,7 @@
 import os
 import yaml
 import glob
-import pytest
+# import pytest
 # import re
 
 import testinfra.utils.ansible_runner
@@ -28,23 +28,22 @@ if os.path.exists(host_dump_var_dir):
         ansible_host_vars[inventory_hostname] = yaml.full_load(open(dump_var_file, 'r'))
 
 
-@pytest.mark.parametrize('name', ['packages1', 'packages2'])
-def test_package(host, name):
-    assert host.package(pkg).is_installed
-
-def test_config(host):
-    f = host.file('/etc/hosts')
-    assert f.exists
-    assert f.is_file
-    assert f.is_directory
-    assert f.is_symlink
-    assert f.linked_to == '/run/lock'
-    assert f.user == 'root'
-    assert f.group == 'root'
-    assert f.mode == 0o600
-    assert f.contains('test string')
-    host_vars = role_vars
-    inventory_hostname = host.ansible.get_variables()['inventory_hostname']
-    if inventory_hostname in ansible_host_vars.keys():
-        host_vars = ansible_host_vars[inventory_hostname]
-    
+# @pytest.mark.parametrize('name', ['packages1', 'packages2'])
+# def test_package(host, name):
+#     assert host.package(pkg).is_installed
+#
+# def test_config(host):
+#     f = host.file('/etc/hosts')
+#     assert f.exists
+#     assert f.is_file
+#     assert f.is_directory
+#     assert f.is_symlink
+#     assert f.linked_to == '/run/lock'
+#     assert f.user == 'root'
+#     assert f.group == 'root'
+#     assert f.mode == 0o600
+#     assert f.contains('test string')
+#     host_vars = role_vars
+#     inventory_hostname = host.ansible.get_variables()['inventory_hostname']
+#     if inventory_hostname in ansible_host_vars.keys():
+#         host_vars = ansible_host_vars[inventory_hostname]
