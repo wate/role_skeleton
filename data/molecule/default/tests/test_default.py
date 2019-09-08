1d04ceb72d500e9ee397d83772ffdbbe51f1932d
wate <pc.poisoning@gmail.com>
2019-09-08 19:42:38 +0900

    update

--- data/molecule/default/tests/test_default.py
+++ data/molecule/default/tests/test_default.py
@@ -1,5 +1,6 @@
 import os
 import yaml
+import glob
 import pytest
 # import re
 
@@ -8,21 +9,26 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-
-property = {}
-default_var_file = os.environ['MOLECULE_PROJECT_DIRECTORY'] + '/defaults/main.yml'
-if os.path.exists(default_var_file):
-    property.update(yaml.full_load(open(default_var_file, 'r')))
+role_vars = role_default_vars = {}
+role_default_var_file = os.environ['MOLECULE_PROJECT_DIRECTORY'] + '/defaults/main.yml'
+if os.path.exists(role_default_var_file):
+    role_default_vars.update(yaml.full_load(open(role_default_var_file, 'r')))
+    role_vars.update(role_default_vars)
 
 role_var_file = os.environ['MOLECULE_PROJECT_DIRECTORY'] + '/vars/main.yml'
 if os.path.exists(role_var_file):
-    property.update(yaml.full_load(open(role_var_file, 'r')))
+    role_vars.update(yaml.full_load(open(role_var_file, 'r')))
+
+ansible_host_vars = {}
+host_dump_var_dir = os.environ['MOLECULE_SCENARIO_DIRECTORY'] + '/dump_vars'
+if os.path.exists(host_dump_var_dir):
+    dump_var_files = glob.glob(host_dump_var_dir + '/*.yml')
+    for dump_var_file in dump_var_files:
+        inventory_hostname = os.path.basename(dump_var_file)[:-4]
+        ansible_host_vars[inventory_hostname] = yaml.full_load(open(dump_var_file, 'r'))
 
 
-@pytest.mark.parametrize('name', [
-    'packages1',
-    'packages2',
-])
+@pytest.mark.parametrize('name', ['packages1', 'packages2'])
 def test_package(host, name):
     assert host.package(pkg).is_installed
 
@@ -37,3 +43,8 @@ def test_config(host):
     assert f.group == 'root'
     assert f.mode == 0o600
     assert f.contains('test string')
+    host_vars = role_vars
+    inventory_hostname = host.ansible.get_variables()['inventory_hostname']
+    if inventory_hostname in ansible_host_vars.keys():
+        host_vars = ansible_host_vars[inventory_hostname]
+    
