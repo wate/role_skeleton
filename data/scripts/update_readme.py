9f23075fadfb036cd5759bd4835eefaabd60f34e
wate <pc.poisoning@gmail.com>
2022-09-21 08:52:50 +0900

    update

--- data/scripts/update_readme.py
+++ data/scripts/update_readme.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from ensurepip import version
 import os
 import re
 import yaml
@@ -6,13 +7,14 @@ import yaml
 DS = os.sep
 ROLE_ROOT = os.path.dirname(os.path.dirname(__file__))
 
-if os.path.exists(ROLE_ROOT + DS + 'README.md'):
-    role_name = os.path.basename(ROLE_ROOT)
-    readme_description = ''
-    readme_platform_text = ''
-    readme_variables_text = ''
-    readme_dependencies_text = ''
-    readme_license_text = ''
+def parse_meta_file():
+    role_info = {
+        'name': os.path.basename(ROLE_ROOT),
+        'description': None,
+        'platforms': {},
+        'license': None,
+        'dependencies': []
+    }
     if os.path.exists(ROLE_ROOT + DS + 'meta' + DS + 'main.yml'):
         role_meta = yaml.safe_load(
             open(ROLE_ROOT + DS + 'meta' + DS + 'main.yml', 'r'))
@@ -20,114 +22,131 @@ if os.path.exists(ROLE_ROOT + DS + 'README.md'):
         if 'galaxy_info' in meta_keys:
             galaxy_info_keys = role_meta['galaxy_info'].keys()
             if 'role_name' in galaxy_info_keys:
-                role_name = role_meta['galaxy_info']['role_name']
+                role_info['name'] = role_meta['galaxy_info']['role_name']
             if 'description' in galaxy_info_keys:
-                readme_description = role_meta['galaxy_info']['description']
+                role_info['description'] = role_meta['galaxy_info']['description']
             if 'license' in galaxy_info_keys:
-                readme_license_text = role_meta['galaxy_info']['license']
+                role_info['license'] = role_meta['galaxy_info']['license']
             if 'platforms' in galaxy_info_keys:
+                platforms = {}
                 for platform in  role_meta['galaxy_info']['platforms']:
-                    readme_platform_text += '### ' + platform['name'] + "\n"
-                    readme_platform_text += "\n"
+                    platform_name = platform['name']
+                    platforms[platform_name] = []
                     if 'versions' in platform.keys() and 'all' not in platform['versions']:
+                        platforms[platform_name] = []
                         for platform_version in platform['versions']:
-                            readme_platform_text += '- ' + platform_version + "\n"
-
+                            platforms[platform_name].append(platform_version)
+                role_info['platforms'] = platforms
         if 'dependencies' in role_meta.keys():
+            role_info['dependencies'] = []
             for dependency in role_meta['dependencies']:
                 if dependency is str:
-                    readme_dependencies_text += '- ' + dependency + "\n"
+                    role_info['dependencies'].append(dependency)
                 else:
                     dependency_keys = dependency.keys()
                     if 'role' in dependency_keys:
-                        readme_dependencies_text += '- ' + dependency['role'] + "\n"
+                        role_info['dependencies'].append(dependency['role'])
                     elif 'name' in dependency_keys:
-                        readme_dependencies_text += '- ' + dependency['name'] + "\n"
+                        role_info['dependencies'].append(dependency['name'])
                     elif 'src' in dependency_keys:
-                        readme_dependencies_text += '- ' + dependency['src'] + "\n"
+                        role_info['dependencies'].append(dependency['src'])
 
+    return role_info
+
+def parse_var_file():
+    var_docs = {}
     if os.path.exists(ROLE_ROOT + DS + 'defaults' + DS + 'main.yml'):
         with open(ROLE_ROOT + DS + 'defaults' + DS + 'main.yml') as fp:
-            var_docs = {}
-            var_name = ''
+            var_name = None
+            doc_block_start = False
+            doc_block_end = False
             doc_block_lines = []
             for line in fp.readlines():
                 if len(line.strip()) > 0:
-                    match = re.search(r'^([a-zA-Z0-9_]+)\s?:\s+', line)
-                    if match:
-                        var_name = match.group(1)
+                    var_name_match = re.search(r'^([a-zA-Z0-9_]+)\s?:\s+', line)
+                    if var_name_match:
+                        var_name = var_name_match.group(1)
                     else:
-                        match = re.search(r'^#\s+(.+)', line)
-                        if match:
-                            doc_block_line = match.group(1)
-                            if not re.search('^-+$', doc_block_line):
-                                doc_block_lines.append(doc_block_line)
+                        doc_block_sep_match = re.search(r'^#\s+-{3,}', line)
+                        if doc_block_sep_match:
+                            if doc_block_start:
+                                doc_block_end = True
+                            else:
+                                doc_block_start = True
+                        else:
+                            doc_block_match = re.search(r'^#\s+(.+)', line)
+                            if doc_block_match and doc_block_start and not doc_block_end:
+                                doc_block_lines.append(doc_block_match.group(1).strip())
+
+                    if var_name:
+                        var_docs[var_name] = "  \n".join(doc_block_lines)
+                        var_name = None
+                        doc_block_start = False
+                        doc_block_end = False
+                        doc_block_lines = []
                 else:
+                    var_name = None
+                    doc_block_start = False
+                    doc_block_end = False
                     doc_block_lines = []
-                    var_name = ''
 
-                if len(var_name) > 0:
-                    var_docs[var_name] = "  \n".join(doc_block_lines)
-                    doc_block_lines = []
-                    var_name = ''
+    return var_docs
 
-            if len(var_docs) > 0:
-                for var_name, var_doc in var_docs.items():
-                    readme_variables_text += '### `' + var_name + "`\n"
-                    readme_variables_text += "\n"
-                    if len(var_doc) > 0:
-                        readme_variables_text += var_doc + "\n"
-                        readme_variables_text += "\n"
+if os.path.exists(ROLE_ROOT + DS + 'README.md'):
+    role_info = parse_meta_file()
+    var_docs = parse_var_file()
 
     # Readmeのテキストを生成
-    readme_text = role_name + "\n"
+    readme_text = role_info['name'] + "\n"
     readme_text += "=================\n"
     readme_text += "\n"
-    if len(readme_description) > 0:
-        readme_text += readme_description + "\n"
+    if role_info['description']:
+        readme_text += role_info['description'] + "\n"
         readme_text += "\n"
 
-    readme_platform_text = readme_platform_text.strip()
-    if len(readme_platform_text) > 0:
+    if len(role_info['platforms']) > 0:
         readme_text += "OS Platform\n"
         readme_text += "-----------------\n"
         readme_text += "\n"
-        readme_text += readme_platform_text + "\n"
-        readme_text += "\n"
+        for platform_name, platform_versions in role_info['platforms'].items():
+            readme_text += '### ' + platform_name + "\n"
+            readme_text += "\n"
+            if len(platform_versions) > 0:
+                readme_text += "\n".join(map(lambda version: '- ' + str(version), platform_versions)) + "\n"
+                readme_text += "\n"
 
-    readme_variables_text = readme_variables_text.strip()
-    if len(readme_variables_text) > 0:
+    if len(var_docs) > 0:
         readme_text += "Role Variables\n"
         readme_text += "--------------\n"
         readme_text += "\n"
-        readme_text += readme_variables_text + "\n"
-        readme_text += "\n"
+        for var_name, var_doc in var_docs.items():
+            readme_text += '### `' + var_name + "`\n"
+            readme_text += "\n"
+            if len(var_doc) > 0:
+                readme_text += var_doc.strip()+ "\n"
+                readme_text += "\n"
 
-    readme_dependencies_text = readme_dependencies_text.strip()
-    if len(readme_dependencies_text) > 0:
+    if len(role_info['dependencies']) > 0:
         readme_text += "Dependencies\n"
         readme_text += "--------------\n"
         readme_text += "\n"
-        readme_text += readme_dependencies_text + "\n"
+        readme_text += "\n".join(map(lambda dependency: "- " + dependency, role_info['dependencies']))
         readme_text += "\n"
-
     readme_text += "Example Playbook\n"
     readme_text += "--------------\n"
     readme_text += "\n"
     readme_text += "```yaml\n"
     readme_text += "- hosts: servers\n"
     readme_text += "  roles:\n"
-    readme_text += "    - role: " + role_name + "\n"
+    readme_text += "    - role: " + role_info['name'] + "\n"
     readme_text += "```\n"
     readme_text += "\n"
-
-    readme_license_text = readme_license_text.strip()
-    if len(readme_license_text) > 0:
+    if role_info['license']:
         readme_text += "License\n"
         readme_text += "--------------\n"
         readme_text += "\n"
-        readme_text += readme_license_text + " License"
-        if readme_license_text == 'Apache':
+        readme_text += role_info['license'] + " License"
+        if role_info['license'] == 'Apache':
             readme_text += " 2.0"
         readme_text += "\n"
 
